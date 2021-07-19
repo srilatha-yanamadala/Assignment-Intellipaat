cca0d1425e3d55cc1e6e072ea66dd745e5cb010e
Ravindranath Chowdary <55086334+srilatha-yanamadala@users.noreply.github.com>
2021-07-19 18:18:27 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import "./App.css";
+
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value,
+  };
+};
 
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitting(true);
+  };
+
+  const handleChange = (event) => {
+    setFormData({
+      name: event.target.name,
+      message: event.target.message,
+      value: event.target.value,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {" "}
+      <form onSubmit={handleSubmit}>
+        <h2>User Form</h2>
+        <fieldset>
+          <label key="idx">
+            Name :
+            <input name="name" onChange={handleChange} />
+          </label>
+          <br />
+          <br />
+          <label>
+            Message :
+            <textarea message="message" onChange={handleChange} />
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+      <br></br> <hr />
+      {submitting && (
+        <div>
+          You are submitting the following:
+          <ul>
+            {Object.entries(formData).map(([name, value]) => (
+              <li key={name}>{value.toString()}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
